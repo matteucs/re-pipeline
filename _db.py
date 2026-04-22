12eef53150c86a96c4ffb3a3f88f920d4635c97c
matteucs <matthew.eucalitto@gmail.com>
2026-04-22 13:48:47 -0400

    Update _db.py

--- _db.py
+++ _db.py
@@ -1,15 +1,7 @@
 import os
 import asyncpg
 
-_pool = None
-
-async def get_pool():
-    global _pool
-    if _pool is None:
-        _pool = await asyncpg.create_pool(
-            dsn=os.environ["DATABASE_URL"],
-            min_size=1,
-            max_size=5,
-            statement_cache_size=0,   # required for Supabase pooler
-        )
-    return _pool
+async def get_connection():
+    db_url = os.environ.get("DATABASE_URL", "")
+    conn = await asyncpg.connect(db_url, statement_cache_size=0)
+    return conn
